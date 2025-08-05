c5568adbd910580f2117ac81faf85c7cd5784ab3
shreyjain11 <mailshreyjain@gmail.com>
2025-08-05 10:28:54 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useState, useRef, useEffect } from "react";
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import "./globals.css";
 import confetti from "canvas-confetti";
 
